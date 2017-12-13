8dbeb8d2c053910980e498bffe55097ee09955f1
AljoschaMeyer <mail@aljoscha-meyer.de>
2017-12-13 13:50:44 +0100

    Implement SubSink

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Multi Producer Sink
 
 Allows multiple different SubSinks to each write to the same Sink.
+
+This deals with interior mutability, since usually only one mutable reference to a Sink is allowed. This also needs to keep a queue of Tasks so that each SubSink as the correct nonblocking behaviour.
+
+On an error, the SubSinks only return a reference to the Error.
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
 extern crate futures;
 
 use futures::{Sink, Poll, StartSend};
+use futures::Async::Ready;
 
 struct MainSink<S> {
-    sink: S,
+    sink: S, // TODO make this a Cell, so that this can be used from start_send and poll_complete, even they only require an immutable reference to a MainSink.
 }
 
 impl<S> MainSink<S> {
     pub fn sub_sink(&self) -> SubSink<S> {
-        unimplemented!()
+        SubSink(&self)
     }
 }
 
@@ -16,26 +17,41 @@ impl<S: Sink> MainSink<S> {
     pub fn into_inner(self) -> (S, Option<S::SinkError>) {
         unimplemented!()
     }
+
+    // Invoked by the SubSink, unlike the Sink trait method, this does not require
+    // a mutable reference.
+    fn start_send(&self, item: S::SinkItem) -> StartSend<S::SinkItem, &S::SinkError> {
+        unimplemented!()
+    }
+
+    // Invoked by the SubSink, unlike the Sink trait method, this does not require
+    // a mutable reference.
+    fn poll_complete(&self) -> Poll<(), &S::SinkError> {
+        unimplemented!()
+    }
 }
 
-struct SubSink<'s, S: 's> {
-    main: &'s MainSink<S>,
-}
+struct SubSink<'s, S: 's>(&'s MainSink<S>);
 
 impl<'s, S: Sink> Sink for SubSink<'s, S> {
     type SinkItem = S::SinkItem;
     type SinkError = &'s S::SinkError;
 
+    /// Start sending on the underlying Sink.
     fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
-        unimplemented!()
+        self.0.start_send(item)
     }
 
+    /// Poll for completion on the underlying Sink.
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
-        unimplemented!()
+        self.0.poll_complete()
     }
 
+    /// Closing a SubSink is a no-op. To actually close the underlying Sink,
+    /// consume the MainSink via `into_inner()` and then invoke close on the Sink
+    /// itself.
     fn close(&mut self) -> Poll<(), Self::SinkError> {
-        unimplemented!()
+        Ok(Ready(())) // no-op
     }
 }
 
